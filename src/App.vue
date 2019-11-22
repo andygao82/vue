741971647b14a81374c44e90444000108d5f56fd
andygao82 <freedomandy82@gmail.com>
2019-11-22 20:49:44 +1100

    first vue push

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <hr>
+      <button class="btn btn-primary">Confirm</button>
+      <button class="btn btn-success">Use</button>
+      <button class="btn btn-danger">Alert</button>
+    <hr>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+@import './assets/custom.scss';
+</style>
